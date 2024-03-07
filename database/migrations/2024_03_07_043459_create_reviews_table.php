33590fd9086c27dae36db5de563302e2acbaf46c
Ahmad Najib <eng.ahmad.najeeb17@gmail.com>
2024-03-07 08:23:22 +0300

    003 Defining the One to Many Relationship

--- database/migrations/2024_03_07_043459_create_reviews_table.php
+++ database/migrations/2024_03_07_043459_create_reviews_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,9 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            // $table->unsignedBigInteger('book_id');
+            // $table->foreign('book_id')->references('id')->on('books')->cascadeOnDelete();
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
             $table->text('review');
             $table->unsignedTinyInteger('rating');
             $table->timestamps();
