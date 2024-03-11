8dea0c5146552e11bfc5cedf80a7ff3169351317
Ahmad Najib <eng.ahmad.najeeb17@gmail.com>
2024-03-11 18:55:22 +0300

    021 Rate Limiting

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -7,9 +7,11 @@ use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public function __construct()
+    {
+        $this->middleware('throttle:reviews')->only(['store']);
+    }
+
     public function index()
     {
         //
